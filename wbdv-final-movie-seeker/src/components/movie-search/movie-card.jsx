432a76467343d216648894c50ae71d8288e353d1
luwei <weilu1994@gmail.com>
2021-04-24 01:11:31 -0700

    edit style

--- wbdv-final-movie-seeker/src/components/movie-search/movie-card.jsx
+++ wbdv-final-movie-seeker/src/components/movie-search/movie-card.jsx
@@ -4,6 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import {Link} from "react-router-dom";
+import Button from "@material-ui/core/Button";
 
 // this is re-rendered whenever the relevant parts of the used data stores change
 const MovieCard = ({
@@ -27,13 +28,20 @@ const MovieCard = ({
             <CardMedia image={Poster} className="media" />
         )}
         <CardContent>
-          <h4>{Title}</h4>
           {movie.details ? (
-              <Link to={"/details/" + imdbID}>
-                <p>Visit detail page</p>
-              </Link>
+              <>
+                <img className="image" src="https://claretalksrubbish.com/wp-content/uploads/Untitled-design-1080x675.jpg"/>
+                <div className="spacing">
+                  <Link to={"/details/" + imdbID}>
+                    <Button variant="outlined">
+                      Visit detail page
+                    </Button>
+                  </Link>
+                </div>
+              </>
           ) : (
               <div>
+                <h4>{Title}</h4>
                 Released: {Year}
               </div>
           )}
--- wbdv-final-movie-seeker/src/components/movie-search/roll-dice.js
+++ wbdv-final-movie-seeker/src/components/movie-search/roll-dice.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import appStore from './app-store';
 import {Link, useParams, useHistory} from 'react-router-dom'
+import Button from "@material-ui/core/Button";
 
 const RollDice = () => {
 
@@ -8,16 +9,21 @@ const RollDice = () => {
 
   const rollDice = () => {
     const list = appStore.movies
-    history.push(`/details/${list[Math.floor((Math.random()*list.length))].imdbID}`)
+    const randomMovie = list[Math.floor((Math.random()*list.length))];
+    if (randomMovie === undefined) {
+      alert("Please choose search a movie first!")
+    } else {
+      history.push(`/details/${randomMovie.imdbID}`)
+    }
   }
 
   return (
       <div>
-        <button
+        <Button
             onClick={() => rollDice()}
             className="btn btn-primary">
-          <i className="fas fa-dice"></i>
-        </button>
+          <i className="fas fa-dice fa-2x"></i>
+        </Button>
       </div>
   )
 }
